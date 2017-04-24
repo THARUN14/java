edac2b2c10dcbae0fc90f334d760ccf1cf667414
THARUN14 <tharun.muthusamy@gmail.com>
2017-04-24 19:04:25 +0530

    Add files via upload

---
+++ bodd.java
@@ -0,0 +1,15 @@
+class bodd
+{
+public static void main(String args[])
+{
+int a=Integer.parseInt(args[0]);
+int b=Integer.parseInt(args[1]);
+for(int i=a+1;i<b;i++)
+{
+if(i%2!=0)
+{
+System.out.println(i);
+}
+}
+}
+}
