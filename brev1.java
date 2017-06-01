c8d2f8cb2f66b15b3233624a89ae1e4e91efba08
THARUN14 <tharun.muthusamy@gmail.com>
2017-06-01 23:13:02 +0530

    Add files via upload

---
+++ brev1.java
@@ -0,0 +1,28 @@
+class brev
+
+{
+
+public static void main(String args[])
+
+
+{
+
+int a=Integer.parseInt(args[0]);
+
+  int b=0;
+
+  while(a>0)
+
+ {b*=10;  
+  b+=a%10;
+
+    a=a/10;
+
+    
+}
+System.out.print(b);
+
+  
+}
+
+}
