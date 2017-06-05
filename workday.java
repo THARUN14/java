ec02eb789c776211aab344d8111106acb43c4996
THARUN14 <tharun.muthusamy@gmail.com>
2017-06-05 22:25:16 +0530

    Add files via upload

---
+++ workday.java
@@ -0,0 +1,14 @@
+class workday
+{
+    public static void main(String args[])
+    {
+        if(args[0].equalsIgnoreCase("Sunday"))
+        {
+            System.out.print("false");
+        }
+        else
+        {
+            System.out.print("true");
+        }
+    }
+}
