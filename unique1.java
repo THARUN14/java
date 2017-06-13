75d34189d6fa6ca4f08223142d8ae701f341cdd6
THARUN14 <tharun.muthusamy@gmail.com>
2017-06-13 20:45:51 +0530

    Add files via upload

---
+++ unique1.java
@@ -0,0 +1,28 @@
+class unique1
+{
+    public static void main(String args[])
+    {
+      for(int i=0;i<args.length;i++)
+      {
+          int count=0;
+          for(int j=0;j<args.length;j++)
+          { 
+             
+              if(i!=j)
+              {
+                  if(args[i].equals(args[j]))
+                  {
+                      count++;
+                  }
+              }
+          }
+          if(count==0)
+          {
+              System.out.print(args[i]);
+          }
+      }
+    }
+}
+
+
+
