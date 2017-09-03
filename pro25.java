c4ca56fe1b1cf9cd6fb341aec4b6f1196f96be0a
THARUN14 <tharun.muthusamy@gmail.com>
2017-09-03 22:23:49 +0530

    Update pro25.java

--- pro25.java
+++ pro25.java
@@ -1,7 +1,7 @@
 import java.lang.reflect.Array;
 import java.util.*;
 
-public class kbit {
+public class pro25 {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int bit = sc.nextInt();
